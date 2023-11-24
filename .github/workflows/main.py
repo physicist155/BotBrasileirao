ac38740c29a9e3ceb9351c9c062c59b4522e5968
Henrique Teixeira <114967021+physicist155@users.noreply.github.com>
2023-11-24 10:14:33 -0300

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -3,15 +3,15 @@ import os
 
 #Instalattion/Access to Twitter account
 
-auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'],os.environ['CONSUMER_SECRET'])
-auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+auth = tweepy.OAuthHandler(os.environ["CONSUMER_KEY"],os.environ["CONSUMER_SECRET"])
+auth.set_access_token(os.environ["ACCESS_TOKEN"], os.environ["ACCESS_TOKEN_SECRET"])
 api = tweepy.API(auth, wait_on_rate_limit = True)
 
 client = tweepy.Client(
-    consumer_key = os.environ['CONSUMER_KEY'],
-    consumer_secret = os.environ['CONSUMER_SECRET'],
-    access_token = os.environ['ACCESS_TOKEN'],
-    access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+    consumer_key = os.environ["CONSUMER_KEY"],
+    consumer_secret = os.environ["CONSUMER_SECRET"],
+    access_token = os.environ["ACCESS_TOKEN"],
+    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 )
 
 
