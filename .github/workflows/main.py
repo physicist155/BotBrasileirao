6a9e63d1945beaf673984c8fac95c390534a78d9
Henrique Teixeira <114967021+physicist155@users.noreply.github.com>
2023-11-23 23:31:01 -0300

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,4 +1,21 @@
-from auth import API_KEY, client, api
+import tweepy
+import os
+
+#Instalattion/Access to Twitter account
+
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'],os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+api = tweepy.API(auth, wait_on_rate_limit = True)
+
+client = tweepy.Client(
+    consumer_key = os.environ['CONSUMER_KEY'],
+    consumer_secret = os.environ['CONSUMER_SECRET'],
+    access_token = os.environ['ACCESS_TOKEN'],
+    access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
+)
+
+
+API_KEY = os.environ.get("API_KEY")
 
 #Função para trocar a foto de perfil da conta
 def profile_image(filename):
