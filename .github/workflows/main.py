5de43a7ee7a447c9c0f6313924edf0bdcb85195d
Henrique Teixeira <114967021+physicist155@users.noreply.github.com>
2023-11-30 17:04:28 -0300

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -13,44 +13,11 @@ if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://bot-brasileirao-default-rtdb.firebaseio.com/'
     })
-def cria_json_firebase():
-  file = open("file.json","w")
-  file.write('{\n')
 
-  file.write('"type": "service_account"')
-  file.write(',\n')
-
-  file.write('"project_id": "bot-brasileirao"')
-  file.write(',\n')
-
-  file.write('"private_key_id": "%s"'%(os.getenv("PRIVATE_KEY_ID")))
-  file.write(',\n')
-
-  file.write('client_email": "%s"'%(os.getenv("CLIENT_EMAIL")))
-  file.write(',\n')
-
-  file.write('"client_id": "%s"'%(os.getenv("CLIENT_ID")))
-  file.write(',\n')
-
-  file.write('"auth_uri": "%s"'%(os.getenv("AUTH_URI")))
-  file.write(',\n')
-
-  file.write('token_uri": "%s"'%(os.getenv("TOKEN_URI")))
-  file.write(',\n')
-
-  file.write('"auth_provider_x509_cert_url": "%s"'%(os.getenv("AUTH_PROVIDER_X500_CERT_URL")))
-  file.write(',\n')
-
-  file.write('"client_x509_cert_url": "%s"'%(os.getenv("CLIENT_X509_CERT_URL")))
-  file.write(',\n')
-
-  file.write('"universe_domain": "%s"'%(os.getenv("UNIVERSE_DOMAIN")))
-  file.write(',\n')
-
-
-  file.write('}\n')
-  file.close()
-
+    
+def post_tweet(text):
+    client.create_tweet(text=text)
+    
 def get_previous_scores():
     ref = db.reference('placares_anteriores')
     return ref.get() or {}
